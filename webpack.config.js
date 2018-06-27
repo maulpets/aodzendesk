174e39ab2ff82eb71621d19daa09618240f34f86
maulpets <maulpets@gmail.com>
2018-06-27 10:55:43 -0400

    working

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,12 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
       },
       {
